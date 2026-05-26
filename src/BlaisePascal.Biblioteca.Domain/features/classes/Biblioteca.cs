f9b40bdb0507c6cb6ec47e1daaf500ffd891b5b6
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-05-26 15:37:23 +0200

    Added Biblioteca class

---
+++ src/BlaisePascal.Biblioteca.Domain/features/classes/Biblioteca.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.Biblioteca.Domain.features.classes
+{
+    public class Biblioteca
+    {
+        private string Nome { set; get; }
+        private string Indirizzo { set; get; }
+        private string OrarioApertura { set; get; }
+        private string OrarioChiusura { set; get; }
+        private List<Libro> Libri { set; get; }
+
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            Libri = new List<Libro>();
+        }
+
+        public int NumeroLibri()
+        {
+            return Libri.Count;
+        }
+
+    }
+}
